3be4e0ac74da3d43424ee422a9e7c4f44d6d0d2e
Lenny <Lennymgr27@gmail.com>
2022-06-04 14:53:41 -0400

    Agregar archivos de proyecto.

---
+++ CRUD/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿
+namespace CRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtboxID = new System.Windows.Forms.Label();
+            this.txtboxNombre = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtboxOcupacion = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.dgvPuebaCRUD = new System.Windows.Forms.DataGridView();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPuebaCRUD)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(263, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Prueba de CRUD";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(115, 77);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // txtboxID
+            // 
+            this.txtboxID.AutoSize = true;
+            this.txtboxID.Location = new System.Drawing.Point(33, 82);
+            this.txtboxID.Name = "txtboxID";
+            this.txtboxID.Size = new System.Drawing.Size(21, 17);
+            this.txtboxID.TabIndex = 2;
+            this.txtboxID.Text = "ID";
+            // 
+            // txtboxNombre
+            // 
+            this.txtboxNombre.AutoSize = true;
+            this.txtboxNombre.Location = new System.Drawing.Point(33, 131);
+            this.txtboxNombre.Name = "txtboxNombre";
+            this.txtboxNombre.Size = new System.Drawing.Size(58, 17);
+            this.txtboxNombre.TabIndex = 4;
+            this.txtboxNombre.Text = "Nombre";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(115, 128);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // txtboxOcupacion
+            // 
+            this.txtboxOcupacion.AutoSize = true;
+            this.txtboxOcupacion.Location = new System.Drawing.Point(33, 187);
+            this.txtboxOcupacion.Name = "txtboxOcupacion";
+            this.txtboxOcupacion.Size = new System.Drawing.Size(76, 17);
+            this.txtboxOcupacion.TabIndex = 6;
+            this.txtboxOcupacion.Text = "Ocupacion";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(115, 187);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // dgvPuebaCRUD
+            // 
+            this.dgvPuebaCRUD.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPuebaCRUD.Location = new System.Drawing.Point(49, 238);
+            this.dgvPuebaCRUD.Name = "dgvPuebaCRUD";
+            this.dgvPuebaCRUD.RowHeadersWidth = 51;
+            this.dgvPuebaCRUD.RowTemplate.Height = 24;
+            this.dgvPuebaCRUD.Size = new System.Drawing.Size(689, 184);
+            this.dgvPuebaCRUD.TabIndex = 7;
+            this.dgvPuebaCRUD.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvPuebaCRUD_CellContentClick);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(382, 82);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(134, 55);
+            this.btnActualizar.TabIndex = 8;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(382, 154);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(134, 55);
+            this.btnGuardar.TabIndex = 9;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(571, 149);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(134, 55);
+            this.btnNuevo.TabIndex = 10;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(571, 82);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(134, 55);
+            this.btnEliminar.TabIndex = 11;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.dgvPuebaCRUD);
+            this.Controls.Add(this.txtboxOcupacion);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.txtboxNombre);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtboxID);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPuebaCRUD)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label txtboxID;
+        private System.Windows.Forms.Label txtboxNombre;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label txtboxOcupacion;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DataGridView dgvPuebaCRUD;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Button btnEliminar;
+    }
+}
+
---
+++ CRUD/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CRUD
+{
+    public partial class Form1 : Form
+    {
+      
+
+        public Form1()
+        {
+            InitializeComponent();
+            llenaTabla();
+        }
+
+        public void llenaTabla()
+        {
+            SqlConnection Conn = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog = PruebaCRUD; Data Source = DESKTOP-BSKLS9C");
+            string consulta = @"SELECT * FROM Profesor";
+            SqlDataAdapter adapter = new SqlDataAdapter(consulta, Conn);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            dgvPuebaCRUD.DataSource = dataTable;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection Conn = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog = PruebaCRUD; Data Source = DESKTOP-BSKLS9C");
+                Conn.Open();
+                string query = @"INSERT INTO Profesor (Nombre, Ocupacion)
+                            VALUES(
+                                    @Nombre,
+                                    @Ocupacion
+                                    )";
+                SqlCommand command = new SqlCommand(query, Conn);
+                command.Parameters.AddWithValue("@ID", int.Parse(textBox1.Text));
+                command.Parameters.AddWithValue("@Nombre", textBox2.Text);
+                command.Parameters.AddWithValue("@Ocupacion", textBox3.Text);
+                command.ExecuteNonQuery();
+                Conn.Close();
+
+                MessageBox.Show("Guardado Exitosamente");
+
+                llenaTabla();
+
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Este es el error  " + err.Message);
+
+            }
+            finally
+            {
+                SqlConnection Conn = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog = PruebaCRUD; Data Source = DESKTOP-BSKLS9C");
+                Conn.Close();
+            }
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection Conn = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog = PruebaCRUD; Data Source = DESKTOP-BSKLS9C");
+                Conn.Open();
+                string query = @"UPDATE Profesor
+                            SET
+                                Nombre=@Nombre,
+                                Ocupacion=@Ocupacion
+                             WHERE ID=@ID";
+                SqlCommand command = new SqlCommand(query, Conn);
+                command.Parameters.AddWithValue("@ID", int.Parse(textBox1.Text));
+                command.Parameters.AddWithValue("@Nombre", textBox2.Text);
+                command.Parameters.AddWithValue("@Ocupacion", textBox3.Text);
+                command.ExecuteNonQuery();
+                Conn.Close();
+
+                MessageBox.Show(" Actualizado exitosamente");
+
+                llenaTabla();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Este es el error  " + err.Message);
+
+            }
+            finally
+            {
+                SqlConnection Conn = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog = PruebaCRUD; Data Source = DESKTOP-BSKLS9C");
+                Conn.Close();
+            }
+        }
+
+        private void dgvPuebaCRUD_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
